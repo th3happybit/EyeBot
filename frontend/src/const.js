7ca847b629b73afd9f3141dfca21cfd0ad465979
roiacult <roiacult27@gmail.com>
2021-07-01 21:18:43 +0100

    prepare object list view

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,7 +1,9 @@
-export const WEB_SOCKET_BASE_URL = 'ws://raspberrypi:8000';
+// export const WEB_SOCKET_BASE_URL = 'ws://raspberrypi:8000';
+export const WEB_SOCKET_BASE_URL = 'ws://localhost:8000';
 
 export const JOYSTICK_WSS_URL = WEB_SOCKET_BASE_URL + '/joystick_ws';
 export const CAM_WSS_URL = WEB_SOCKET_BASE_URL + '/camera_ws';
+export const OBJECTS_WSS_URL = WEB_SOCKET_BASE_URL + '/objects_ws';
 
 // export const CAM_SERVER = 'http://raspberrypi:8082';
 export const CAM_SERVER = 'https://www.youtube.com/embed/jgnkQ2f84og';
